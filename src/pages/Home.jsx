91f76d62ab52e16fa8269623b870117939843c37
khushiiiii24 <khushisogani24@gmail.com>
2025-06-02 20:47:51 +0530

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 function Home() {
+  const[dayData,setDayData]=useState([]);
+  useEffect(()=>{
+    fetchData();
+  },[]);
+  async function fetchData() {
+    const response=await axios.get(" https://api.themoviedb.org/3/trending/movie/day?language=en-US");
+    console.log(response);
+    setDayData(response.data);
+  }
   return (
     <div>
-       https://api.themoviedb.org/3/trending/movie/day?language=en-US
-
+      <h1>Trending movies by day</h1>
+      <div></div>
     </div>
   )
 }
